9bfe53b1f0ce5823314ce4b9314ef77a156fc21b
Surya <ssurya5599@gmail.com>
2021-01-29 13:57:39 -0800

    completed part 2

--- lab3_part1.sql
+++ lab3_part1.sql
@@ -1,5 +1,9 @@
 
 -----------------------------------PART 1-------------------------------
+--using 10 for dates because of ex. 01/01/2020
+--using 11 for SSN because 111-11-1111
+--using 30 for strings, from the notes
+
 DROP TABLE IF EXISTS advise CASCADE;
 DROP TABLE IF EXISTS work_proj CASCADE;
 DROP TABLE IF EXISTS work_in CASCADE;
@@ -73,15 +77,61 @@ CREATE TABLE advise	(g_SSN CHAR(11),
 			FOREIGN KEY (s_SSN) REFERENCES Graduate(SSN));
 
 ---------------------------------PART 2----------------------------------------
+DROP TABLE IF EXISTS Lives CASCADE;
+DROP TABLE IF EXISTS Home CASCADE;
+DROP TABLE IF EXISTS Perform CASCADE;
+DROP TABLE IF EXISTS Plays CASCADE;
+DROP TABLE IF EXISTS Instrument CASCADE;
+DROP TABLE IF EXISTS Songs CASCADE;
+DROP TABLE IF EXISTS Album CASCADE;
+DROP TABLE IF EXISTS Musicians CASCADE;
 
 
-CREATE TABLE Musicians	(ssn CHAR(11) NOT NULL;
-			name 
+
+CREATE TABLE Musicians	(ssn CHAR(11) NOT NULL,
+			name CHAR(30) NOT NULL,
+			PRIMARY KEY (ssn));
+
+--Create producer by giving musician ssn
+CREATE TABLE Album	(albumIdentifier INTEGER,
+			copyrightDate CHAR(10) NOT NULL,
+			speed INTEGER,
+			title CHAR(30) NOT NULL,
+			ssn CHAR(30),
+			PRIMARY KEY (albumIdentifier),
+			FOREIGN KEY (ssn) REFERENCES Musicians(ssn));
+
+CREATE TABLE Songs	(songId INTEGER,
+			title CHAR(30),
+			author CHAR(30),
+			albumIdentifier INTEGER,
+			PRIMARY KEY (songId),
+			FOREIGN KEY (albumIdentifier) REFERENCES Album(albumIdentifier));
+
+CREATE TABLE Instrument	(instrId INTEGER,
+			dname CHAR(30),
+			key CHAR(30),
+			PRIMARY KEY (instrID));
+
+CREATE TABLE Plays	(instrID INTEGER,
+			ssn CHAR(11),
+			PRIMARY KEY (instrId, ssn),
+			FOREIGN KEY (ssn) REFERENCES Musicians(ssn),
+			FOREIGN KEY (instrID) REFERENCES Instrument(instrId));
+			
+CREATE TABLE Perform	(songId INTEGER,
+			ssn CHAR(11),
+			PRIMARY KEY (songId, ssn),
+			FOREIGN KEY (ssn) REFERENCES Musicians(ssn),
+			FOREIGN KEY (songId) REFERENCES Songs(songId));
+
+CREATE TABLE Home	(address CHAR(30),
+			phone_no CHAR(10),
+			PRIMARY KEY(address));
 
 
-
-
-
-
-
-
+CREATE TABLE Lives	(address CHAR(30),
+			ssn CHAR(11),
+			PRIMARY KEY (ssn, address),
+			FOREIGN KEY (ssn) REFERENCES Musicians(ssn),
+			FOREIGN KEY (address) REFERENCES Home(address));
