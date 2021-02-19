05d670703e7ee7ad3182e6effdbc8e6f5311d6aa
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2021-02-19 14:00:44 -0800

    Update queries.sql

--- lab6/lab6/queries.sql
+++ lab6/lab6/queries.sql
@@ -2,6 +2,5 @@ SELECT sname, count(*) FROM Suppliers JOIN Catalog ON Catalog.sid = Suppliers.si
 
 SELECT sname, count(*) as "num of parts" FROM Suppliers JOIN Catalog ON Catalog.sid = Suppliers.sid JOIN Parts ON Parts.pid = Catalog.pid GROUP by sname HAVING count(*) >= 3;
 
-SELECT sname, count(*) FROM Suppliers JOIN Catalog ON Catalog.sid = Suppliers.sid JOIN Parts ON Parts.pid = Catalog.pid WHERE sname = ALL (SELECT sname FROM Suppliers WHERE sid = ALL (SELECT sid FROM Catalog WHERE pid = ALL (SELECT pid FROM PARTS where color = 'Green'))) GROUP by sname;
-
+SELECT S.sname, COUNT(*) FROM Suppliers S, Parts P, Catalog C WHERE P.pid = C.pid AND S.sid = C.sid AND S.sid IN (SELECT S.sid FROM Suppliers S, Parts P, Catalog C WHERE P.pid = C.pid AND S.sid = C.sid AND P.color = 'Green' EXCEPT SELECT S.sid FROM Suppliers S, Parts P, Catalog C WHERE P.pid = C.pid AND S.sid = C.sid AND P.color != 'Green') GROUP BY S.sid;
 SELECT sname,max FROM Suppliers JOIN (SELECT sid,Max(cost) From Catalog WHERE sid = ANY (SELECT C.sid From Catalog C, Parts P WHERE C.pid = P.pid and P.color = 'Red' INTERSECT SELECT C1.sid FROM Catalog C1, Parts P1 WHERE C1.pid = P1.pid AND P1.color = 'Green') GROUP BY sid) AS foo ON Suppliers.sid = foo.sid;
