b4dc28f0973ca16ec9b95f494c0dcd35d204e3c2
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2021-02-19 12:34:49 -0800

    Update EmbeddedSQL.java

--- lab6/lab6/EmbeddedSQL.java
+++ lab6/lab6/EmbeddedSQL.java
@@ -271,15 +271,24 @@ public class EmbeddedSQL {
    }//end Query2
 
    public static void Query3(EmbeddedSQL esql){
-      // Your code goes here.
-      // ...
-      // ...
+     try{
+         String query = "SELECT sname, count(*) FROM Suppliers JOIN Catalog ON Catalog.sid = Suppliers.sid JOIN Parts ON Parts.pid = Catalog.pid WHERE sname = ALL (SELECT sname FROM Suppliers WHERE sid = ALL (SELECT sid FROM Catalog WHERE pid = ALL (SELECT pid FROM PARTS where color = 'Green'))) GROUP by sname;";
+         int rowCount = esql.executeQuery(query);
+         System.out.println ("total row(s): " + rowCount);
+      }catch(Exception e){
+         System.err.println (e.getMessage());
+      }
    }//end Query3
 
    public static void Query4(EmbeddedSQL esql){
-      // Your code goes here.
-      // ...
-      // ...
+      try{
+         String query = "SELECT sname,max FROM Suppliers JOIN (SELECT sid,Max(cost) From Catalog WHERE sid = ANY (SELECT C.sid From Catalog C, Parts P WHERE C.pid = P.pid and P.color = 'Red' INTERSECT SELECT C1.sid FROM Catalog C1, Parts P1 WHERE C1.pid = P1.pid AND P1.color = 'Green') GROUP BY sid) AS foo ON Suppliers.sid = foo.sid;
+";
+         int rowCount = esql.executeQuery(query);
+         System.out.println ("total row(s): " + rowCount);
+      }catch(Exception e){
+         System.err.println (e.getMessage());
+      }
    }//end Query4
 
    public static void Query5(EmbeddedSQL esql){
